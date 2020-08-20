2e89f97e947c174d7a8e2b9398b7fed25be52ac4
nelcel <nelsonklojo@yahoo.com>
2020-08-19 19:36:26 -0700

    Light UI improvements on team score reports

--- server-side/scoreboard-server/www/score_report.js
+++ server-side/scoreboard-server/www/score_report.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', (event) => {
 
     const newLocal = 750;
-    var chart = new CanvasJS.Chart("Scoreboard", {
+    var chart = new CanvasJS.Chart("score_graph", {
         
         animationEnabled: true,
         theme: "cyan2",
@@ -17,8 +17,8 @@ window.addEventListener('load', (event) => {
             labelMaxWidth: 200
         },
         axisY:{
-           includeZero: false,
-           maximum: 100
+        includeZero: false,
+        maximum: 100
         },
         data: [{        
             type: "line",
@@ -27,17 +27,17 @@ window.addEventListener('load', (event) => {
             dataPoints: [
                 { y: 8, label: "12/07/19  09:00" },
                 { y: 14, label: "12/07/19" },
-			    { y: 21, label: "12/07/19" },
+                { y: 21, label: "12/07/19" },
                 { y: 34, label: "12/07/19"},
                 { y: 40, label: "12/07/19" },
                 { y: 47, label: "12/07/19" },
-			    { y: 54, label: "12/07/19"},
+                { y: 54, label: "12/07/19"},
                 { y: 46, label: "12/07/19" },
                 { y: 56, label: "12/07/19" },
-			    { y: 64, label: "12/07/19" },
-			    { y: 68, label: "12/07/19" },
-			    { y: 75, label: "12/07/19" },
-			    { y: 88, label: "12/07/19" },
+                { y: 64, label: "12/07/19" },
+                { y: 68, label: "12/07/19" },
+                { y: 75, label: "12/07/19" },
+                { y: 88, label: "12/07/19" },
                 { y: 90, label: "12/07/19" },
                 { y: 90, label: "12/07/19" }
             ]
@@ -49,32 +49,30 @@ window.addEventListener('load', (event) => {
             dataPoints: [
                 { y: 5, label: "12/07/19  09:00" },
                 { y: 10, label: "12/07/19" },
-			    { y: 16, label: "12/07/19" },
+                { y: 16, label: "12/07/19" },
                 { y: 24, label: "12/07/19"},
-                { y: 30, label: "12/07/19" },
+                { y: -30, label: "12/07/19" },
                 { y: 40, label: "12/07/19" },
-			    { y: 48, label: "12/07/19"},
+                { y: 48, label: "12/07/19"},
                 { y: 56, label: "12/07/19" },
                 { y: 62, label: "12/07/19" },
-			    { y: 64, label: "12/07/19" },
-			    { y: 70, label: "12/07/19" },
-			    { y: 72, label: "12/07/19" },
-			    { y: 76, label: "12/07/19" },
+                { y: 64, label: "12/07/19" },
+                { y: 70, label: "12/07/19" },
+                { y: 72, label: "12/07/19" },
+                { y: 76, label: "12/07/19" },
                 { y: 82, label: "12/07/19" },
                 { y: 84, label: "12/07/19" }
             ]
         }
     ]
     });
-    chart.render();
+    chart.render(); 
+    //{ y: 84 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" }, lowest score
+    //{ y: 34, indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" }, highest score
+
 
     gentime = document.getElementById("gentime")
     if (gentime) {
         gentime.innerHTML = (new Date()).toISOString().replace('T', ' ').replace('Z', ' UTC');
     }
-
-    //{ y: 84 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" }, lowest score
-    //			    { y: 34, indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" }, highest score
 });
-
-//14 time stamps sideways
