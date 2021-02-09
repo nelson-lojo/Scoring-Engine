af10ad0e294de5be855d0914a9b1865260ef2b9d
Jeremy Chow <jeremychow02@gmail.com>
2021-02-09 01:53:05 -0800

    File rename + put js at header

--- server-side/scoreboard-server/www/score_report.js
+++ server-side/scoreboard-server/www/score_report.js
@@ -1,147 +1,211 @@
-//parsing the external json file here
-function getInfo() {
-    var fetch = new XMLHttpRequest();
-    fetch.open(
-        'GET', 
-        window.location.host 
-        + '/info/' 
-        + window.location.pathname.split('/')[2], // grab the team ObjectID from the current URL
-        true
-    );
-    fetch.setRequestHeader('Content-type', 'text/plain');
-    fetch.onload = (e) => {
-        if (fetch.status == 200) {
-            team = JSON.parse(fetch.responseText);
-            console.log(team)
-            // continue updating the site
+console.log("PhatBoi was here");
+var parentDiv;
+var cFrame;
+var canvas;
+var widthOffset;
+var heightOffset;
+var canvasWidth;
+var canvasHeight;
 
-        }
-    }
-}
-getInfo()
+var graphX;
+var graphY;
 
-function loadTeams(amount) {
-    var fetch = new XMLHttpRequest();
-    fetch.open('POST', window.location.href + '/teams?competition=' + competition + '&division=' + division, true); 
-    fetch.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    fetch.onload = (e) => {
-        if(fetch.status == 200) {
-            content = JSON.parse(fetch.responseText);
-            table = document.getElementsByTagName('tbody.')[0];
-            // build table with teams
-            for (team of content) {
-                team.startTime = new Date( Date.parse(team.startTime) );
-                if (!team.hasOwnProperty('endTime')) {
-                    team.endTime = new Date();
-                } 
-                playtime = team.endTime.getTime() - team.startTime.getTime();  // make sure these are date objects #* 
-                row = document.createElement("TR")
-                row.addEventListener('click', (event) => {
-                    window.location="team/" + team._id
-                }) 
-                uid = document.createElement("TD");
-                uid.innerHTML = team.uid;
 
-                comp = document.createElement("TD");
-                comp.innerHTML = team.competition;
+window.addEventListener('load', (event) => {
+    parentDiv = document.getElementById("score_graph");
+    cFrame = document.createElement('canvas');
+    cFrame.setAttribute("id", "scoreboard");
+    cFrame.setAttribute("width", parentDiv.clientWidth);
+    cFrame.setAttribute("height", parentDiv.clientHeight);
+    cFrame.style.border = "none";
+    parentDiv.appendChild(cFrame);
+    
+    canvas = cFrame.getContext("2d");
+    widthOffset = 35.0;
+    heightOffset = 20.0 + 80 ;
+    canvasWidth = cFrame.width;
+    canvasHeight = cFrame.height;
+    graphX = 35.0;
+    graphY = 20.0;
+    
+    window.setInterval(update, 10 * 1000);
+    update();
+});
 
-                div = document.createElement("TD");
-                div.innerHTML = team.division;
 
-                playtyme = document.createElement("TD");
-                playtyme.innerHTML =  Math.floor(playtime / (1000 * 3600)) + ':' + Math.floor(playtime / (1000 * 60) % 60);
-                
-                score = document.createElement("TD");
-                score.innerHTML = team.score
-                
-                row.appendChild(uid);
-                row.appendChild(comp);
-                row.appendChild(div);
-                row.appendChild(playtyme);
-                row.appendChild(score);
-                
-                console.log(["Loaded team:", team]);
-                table.appendChild(row)
+function update(){
+
+    var xmlhttp = new XMLHttpRequest();
+    var url = "/info/"+window.location.pathname.split('/')[2];
+    var entry;
+    xmlhttp.onload = function() {
+        if (this.status == 200) {
+            var width = canvasWidth - widthOffset;
+            var height = canvasHeight - heightOffset;
+            entry = JSON.parse(this.responseText);
+            
+            //Team Info Summary
+            var tbody = document.getElementById("teamInfoSummary");
+            tbody.innerHTML = "";
+            var tableRow = document.createElement("tr");
+            var _id = document.createElement("td");
+            var _div = document.createElement("td");
+            var _im_no = document.createElement("td");
+            var _play_time = document.createElement("td");
+            var _total_score = document.createElement("td");
+            var _warnings = document.createElement("td");
+            _id.innerHTML = entry.num;
+            _div.innerHTML = entry.division;
+            _im_no.innerHTML = "" + entry.images.length;
+            var passedTime;
+            if(entry.hasOwnProperty("endTime")){
+                passedTime = new Date(entry.endTime) - new Date(entry.startTime);
+            }else{
+                passedTime = new Date() - new Date(entry.startTime);
             }
+            passedTime /= 1000.0;
+            _play_time.innerHTML = Math.floor(passedTime / 3600) + ":" + Math.floor((passedTime % 3600) / 60);
+            _total_score.innerHTML = "" + entry.score;
+            _warnings.innerHTML = "TODO";
+            tableRow.appendChild(_id);
+            tableRow.appendChild(_div);
+            tableRow.appendChild(_im_no);
+            tableRow.appendChild(_play_time);
+            tableRow.appendChild(_total_score);
+            tableRow.appendChild(_warnings);
+            tbody.appendChild(tableRow);
+            
+            //Image List
+            var iList = document.getElementById("imageList");
+            iList.innerHTML = "";
+            for(var i = 0; i < entry.images.length; i++){
+                var curr_img = entry.images[i];
+                var _tr = document.createElement("tr");
+                var _img_name = document.createElement("td");
+                var _img_time = document.createElement("td");
+                var _img_found = document.createElement("td");
+                var _img_remaining = document.createElement("td");
+                var _img_penalties = document.createElement("td");
+                var _img_score = document.createElement("td");
+                var _img_warn = document.createElement("td");
+                _img_name.innerHTML = curr_img.name;
+                if(curr_img.hasOwnProperty("endTime")){
+                    passedTime = new Date(curr_img.endTime) - new Date(curr_img.startTime);
+                }else{
+                    passedTime = new Date() - new Date(curr_img.startTime);
+                }
+                passedTime /= 1000.0;
+                _img_time.innerHTML = Math.floor(passedTime / 3600) + ":" + Math.floor((passedTime % 3600) / 60);
+                _img_found.innerHTML = curr_img.vulns;
+                _img_remaining.innerHTML = "TODO";
+                _img_penalties.innerHTML = "TODO";
+                _img_score.innerHTML = "" + curr_img.score;
+                _img_warn.innerHTML = "TODO";
+                _tr.appendChild(_img_name);
+                _tr.appendChild(_img_time);
+                _tr.appendChild(_img_found);
+                _tr.appendChild(_img_remaining);
+                _tr.appendChild(_img_penalties);
+                _tr.appendChild(_img_score);
+                _tr.appendChild(_img_warn);
+                iList.appendChild(_tr);
+            }
+            
+            //Setup
+            
+            var images = entry.images;
+            canvas.clearRect(0, 0, canvasWidth, canvasHeight);
+            
+            var startTime = new Date(entry.startTime).getTime();
+            var endTime = new Date(entry.endTime).getTime();
+            
+            var lineColors = ["#FF0000", "#E6E600", "#0000FF", "#00FF00", "#FF9900", "#FF00FF"];
+            
+            //Legend
+            var legendLength = 0;
+            for(var i = 0; i < images.length; i++){
+                var name = images[i].name;
+                canvas.fillStyle = lineColors[i % lineColors.length];
+                canvas.fillRect(canvasWidth - 20, 20 * i + canvasHeight / 4, 10, 10);
+                canvas.font = "15px Arial";
+                canvas.textAlign = 'right';
+                var cms = canvas.measureText(name).width;
+                if(cms > legendLength)
+                    legendLength = cms;
+                canvas.fillText(name, canvasWidth - 30, 10 + 20 * i + canvasHeight / 4);
+            }
+            legendLength += 35;
+            width -= legendLength;
+            canvas.lineWidth = 1;
+            
+            //Graph Section
+            
+            var maxScore = 0;
+            for(var i = 0; i < images.length; i++){
+                for(var j = 0; j < images[i].scores.length; j++){
+                    if (images[i].scores[j].score > maxScore){
+                        maxScore = images[i].scores[j].score;
+                    }
+                }
+            }
+            maxScore = Math.max(40, Math.ceil(maxScore / 10) * 10);
+            canvas.strokeStyle = "#b5b5b5";
+            for(var i = 0; i <= maxScore / 10; i++){
+                canvas.beginPath();
+                var drawHeight = height - (10 * height / maxScore) * i;
+                canvas.moveTo(0 + graphX, drawHeight + graphY);
+                canvas.lineTo(width + graphX, drawHeight + graphY);
+                canvas.stroke();
+            }
+            canvas.lineWidth = 3;
+            for(var i = 0; i < images.length; i++){
+                canvas.strokeStyle = lineColors[i % lineColors.length];
+                canvas.beginPath();
+                canvas.moveTo(0 + graphX, height + graphY);
+                for(var j = 0; j < images[i].scores.length; j++){
+                    var mapHeight = map(images[i].scores[j].score, 0, maxScore, 0, height);
+                    mapHeight = height - mapHeight;
+                    var scoreTime = new Date(entry.images[i].scores[j].time).getTime();
+                    var mapWidth = map(scoreTime, startTime, endTime, 0, width);
+                    canvas.lineTo(mapWidth + graphX, mapHeight + graphY);
+                }
+                canvas.stroke();
+            }
+            
+            //Labeling Section
+            canvas.font = "15px Arial";
+            canvas.textAlign = 'right';
+            canvas.fillStyle = "#000000";
+            for(var i = 0; i <= maxScore / 10; i++){
+                canvas.fillText("" + i * 10, graphX - 10,  height - (10 * height / maxScore) * i + graphY - 1);
+            }
+            var labelAmount = 10;
+            for(var i = 1; i <= labelAmount; i++){
+                canvas.save();
+                var dateTime = new Date(map(i, 0, labelAmount, startTime, endTime));
+                var timeString = dateTime.toLocaleTimeString();
+                canvas.translate(width * i / labelAmount + graphX, height + graphY);
+                var tickLength = 5;
+                canvas.beginPath();
+                canvas.strokeStyle = "#b5b5b5"
+                canvas.moveTo(0, tickLength / 2);
+                canvas.lineTo(0, -tickLength / 2);
+                canvas.stroke();
+                canvas.textAlign = 'right';
+                canvas.rotate(-Math.PI/4);
+                canvas.fillText(timeString, -15, 15);
+                canvas.restore();
+            }
+            //Update
+            var timeUpdate = document.getElementById("gentime");
+            timeUpdate.innerHTML = "" + new Date().toUTCString();
         }
     };
+    xmlhttp.open("GET", url);
+    xmlhttp.send();
+    
 }
-/*
-window.addEventListener('load', (event) => {
 
-    const newLocal = 750;
-    var chart = new CanvasJS.Chart("score_graph", {
-        
-        animationEnabled: true,
-        theme: "cyan2",
-        title:{
-            text: "Image scores",
-            verticalAlign: "top",
-            horizontalAlign: "left",
-            fontFamily: "calibri",
-            fontColor: "black"
-        },
-        axisX:{
-            labelAngle: 145,
-            labelMaxWidth: 200
-        },
-        axisY:{
-        includeZero: false,
-        maximum: 100
-        },
-        data: [{        
-            type: "line",
-            color: "orange",
-            indexLabelFontSize: 12,
-            dataPoints: [
-                { y: 8, label: "12/07/19  09:00" },
-                { y: 14, label: "12/07/19" },
-                { y: 21, label: "12/07/19" },
-                { y: 34, label: "12/07/19"},
-                { y: 40, label: "12/07/19" },
-                { y: 47, label: "12/07/19" },
-                { y: 54, label: "12/07/19"},
-                { y: 46, label: "12/07/19" },
-                { y: 56, label: "12/07/19" },
-                { y: 64, label: "12/07/19" },
-                { y: 68, label: "12/07/19" },
-                { y: 75, label: "12/07/19" },
-                { y: 88, label: "12/07/19" },
-                { y: 90, label: "12/07/19" },
-                { y: 90, label: "12/07/19" }
-            ]
-        },
-        {
-            type: "line",
-            color: "blue",
-            indexLabelFontSize: 13,
-            dataPoints: [
-                { y: 5, label: "12/07/19  09:00" },
-                { y: 10, label: "12/07/19" },
-                { y: 16, label: "12/07/19" },
-                { y: 24, label: "12/07/19"},
-                { y: -30, label: "12/07/19" },
-                { y: 40, label: "12/07/19" },
-                { y: 48, label: "12/07/19"},
-                { y: 56, label: "12/07/19" },
-                { y: 62, label: "12/07/19" },
-                { y: 64, label: "12/07/19" },
-                { y: 70, label: "12/07/19" },
-                { y: 72, label: "12/07/19" },
-                { y: 76, label: "12/07/19" },
-                { y: 82, label: "12/07/19" },
-                { y: 84, label: "12/07/19" }
-            ]
-        }
-    ]
-    });
-    chart.render(); 
-    //{ y: 84 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" }, lowest score
-    //{ y: 34, indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" }, highest score
-
-
-    gentime = document.getElementById("gentime")
-    if (gentime) {
-        gentime.innerHTML = (new Date()).toISOString().replace('T', ' ').replace('Z', ' UTC');
-    }
-});*/
+function map(v, a, b, c, d){
+    return c + (v - a) * (d - c) / (b - a);
+}
